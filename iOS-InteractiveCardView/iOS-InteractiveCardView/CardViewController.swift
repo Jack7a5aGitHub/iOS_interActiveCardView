f489aff3741068e5b45881cfde0287c1d631c958
Jack Wong <jack7a5a1993@icloud.com>
2018-11-11 23:50:50 +0900

    project rdready

---
+++ iOS-InteractiveCardView/iOS-InteractiveCardView/CardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CardViewController.swift
+//  iOS-InteractiveCardView
+//
+//  Created by Jack Wong on 2018/11/11.
+//  Copyright © 2018 Jack Wong. All rights reserved.
+//
+
+import UIKit
+
+final class CardViewController: UIViewController {
+
+    @IBOutlet var handleArea: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ iOS-InteractiveCardView/iOS-InteractiveCardView/MainViewController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS-InteractiveCardView
+//
+//  Created by Jack Wong on 2018/11/11.
+//  Copyright © 2018 Jack Wong. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    private var cardViewController: CardViewController?
+    private var visualEffectView: UIVisualEffectView?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+    }
+
+}
+
